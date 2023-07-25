2fe56b82c0eef7df15707469f0574236fe75f43f
TinoJoel Muchenje <31763088+Tinomuchenje@users.noreply.github.com>
2023-07-25 20:50:18 +0200

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/tinotendamuchenje/development/opentableclone/app/page": [
+      "/Users/tinotendamuchenje/development/opentableclone/app/globals.css",
+      "/Users/tinotendamuchenje/development/opentableclone/node_modules/react-datepicker/dist/react-datepicker.css"
+    ]
+  },
+  "/Users/tinotendamuchenje/development/opentableclone/app/layout.tsx": [
+    "/Users/tinotendamuchenje/development/opentableclone/app/globals.css",
+    "/Users/tinotendamuchenje/development/opentableclone/node_modules/react-datepicker/dist/react-datepicker.css"
+  ]
+}
